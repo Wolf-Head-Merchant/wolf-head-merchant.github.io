dce45ec2318f877f6c3dfd5f14ba8b2d7f8da4a4
WolfHeadMerchant <wolfheadmerchant@gmail.com>
2016-09-16 09:37:52 -0700

    vid play length

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,40 @@
 
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').focus()
-})
+//$('#myModal').on('shown.bs.modal', function () {
+//  $('#myInput').focus()
+//})
 
 var $myCarousel = $("#myCarousel");
 $myCarousel.on("slide.bs.carousel", function (event) {
 	var $currentSlide = $myCarousel.find(".active iframe");
 
-	// exit if there's no iframe, i.e. if this is just an image and not a video player
+
 	if (!$currentSlide.length) { return; }
 
-	// pass that iframe into Froogaloop, and call api("pause") on it.
+
 	var player = Froogaloop($currentSlide[0]);
 	player.api("pause");
 });
+
+var t;
+
+var start = $('#myCarousel').find('.active').attr('data-interval');
+t = setTimeout("$('#myCarousel').carousel({interval: 1000});", start-1000);
+
+$('#myCarousel').on('slid.bs.carousel', function () {
+     clearTimeout(t);
+     var duration = $(this).find('.active').attr('data-interval');
+
+     $('#myCarousel').carousel('pause');
+     t = setTimeout("$('#myCarousel').carousel();", duration-1000);
+})
+
+$('.carousel-control.right').on('click', function(){
+    clearTimeout(t);
+});
+
+$('.carousel-control.left').on('click', function(){
+    clearTimeout(t);
+});
+
+var start = $('#myCarousel').find('.active').attr('data-interval');
+t = setTimeout("$('#myCarousel').carousel({interval: 1000});", start-1000);
