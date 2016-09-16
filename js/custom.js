029231799e7db81cba87c22956d3901bfe929515
WolfHeadMerchant <wolfheadmerchant@gmail.com>
2016-09-16 09:49:53 -0700

    remove carousel imgs

--- js/custom.js
+++ js/custom.js
@@ -14,27 +14,3 @@ $myCarousel.on("slide.bs.carousel", function (event) {
 	var player = Froogaloop($currentSlide[0]);
 	player.api("pause");
 });
-
-var t;
-
-var start = $('#myCarousel').find('.active').attr('data-interval');
-t = setTimeout("$('#myCarousel').carousel({interval: 1000});", start-1000);
-
-$('#myCarousel').on('slid.bs.carousel', function () {
-     clearTimeout(t);
-     var duration = $(this).find('.active').attr('data-interval');
-
-     $('#myCarousel').carousel('pause');
-     t = setTimeout("$('#myCarousel').carousel();", duration-1000);
-})
-
-$('.carousel-control.right').on('click', function(){
-    clearTimeout(t);
-});
-
-$('.carousel-control.left').on('click', function(){
-    clearTimeout(t);
-});
-
-var start = $('#myCarousel').find('.active').attr('data-interval');
-t = setTimeout("$('#myCarousel').carousel({interval: 1000});", start-1000);
